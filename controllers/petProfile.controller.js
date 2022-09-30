6748783e305a5e89772c787db8b647c9d2e4079b
Emily Davidson <emilyabigaildavidson@gmail.com>
2022-09-30 14:02:00 -0400

    endpoints location/petprofile

--- controllers/petProfile.controller.js
+++ controllers/petProfile.controller.js
@@ -3,7 +3,6 @@ const router = require("express").Router();
 const PetProfile = require("../models/petProfile.model");
 
 router.post("/add", async (req, res) => {
-  // res.json({ message: "location" });
   const { userid, dogName, breed, age, dogPic, dogBio, disable } =
     req.body.petProfile;
 
@@ -20,6 +19,67 @@ router.post("/add", async (req, res) => {
   try {
     const newPetProfile = await petProfile.save();
     res.json({ petProfile: newPetProfile });
+  } catch (error) {
+    res.json({ message: error.message });
+  }
+});
+
+//GET:
+router.get("/", async (req, res) => {
+  try {
+    const petProfile = await PetProfile.find();
+    res.json({ petProfile: petProfile });
+  } catch (error) {
+    res.json({ message: error.message });
+  }
+});
+
+//delete
+router.delete("/:id", async (req, res) => {
+  console.log(req.params);
+  try {
+    const deletedPetProfile = await PetProfile.deleteOne({
+      _id: req.params.id,
+    });
+    res.json({
+      message:
+        deletedPetProfile.deletedCount > 0
+          ? "petProfile removed"
+          : "petProfile not found",
+
+      deletedPetProfile: deletedPetProfile,
+    });
+  } catch (error) {
+    res.json({ message: error.message });
+  }
+});
+
+//update
+router.patch("/update/:id", async (req, res) => {
+  console.log(req.params);
+  try {
+    const filter = { _id: req.params.id };
+    const dataToReplace = req.body.petProfile;
+    const returnOptions = {
+      returnOriginal: false,
+    };
+    const petProfile = await PetProfile.findOneAndUpdate(
+      filter,
+      dataToReplace,
+      returnOptions
+    );
+
+    res.json({ message: "petProfile updated", petProfile: petProfile });
+  } catch (error) {
+    res.json({ message: error.message });
+  }
+});
+
+//get
+router.get("/:id", async (req, res) => {
+  try {
+    const petProfile = await PetProfile.findById(req.params.id);
+    res.json({ petProfile: petProfile });
   } catch (error) {
     res.json({ message: error.message });
   }
