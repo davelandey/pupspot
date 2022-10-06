3c6551c1375a1a4dc2c628580b2b58714937dd45
Emily Davidson <emilyabigaildavidson@gmail.com>
2022-10-06 16:50:30 -0400

    Thursday

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,10 +1,29 @@
+import React from "react";
+import Signup from "./Signup.jsx";
+import { Col, Row, Container } from "reactstrap";
+
 const Footer = (props) => {
-    return (
-    
-    <>
-    <h1>Hello from footer</h1>
-    </>  
-    );
-}
- 
+  //Current year for copywrite:
+  const currentYear = new Date().getFullYear();
+  const yearTxt = currentYear === 2022 ? "2022" : "2022 - " + currentYear;
+
+  return (
+    <div className="footer">
+      <Container>
+        <Row>
+          <Col className="copyright"> PupSpot © {yearTxt}</Col>
+          <Col className="mid"></Col>
+
+          <Col className="signUpLink">
+            {" "}
+            <a style={{ textDecoration: "none", color: "white" }} href="Signup">
+              <span style={{ fontWeight: "bold" }}>SIGNUP</span>
+            </a>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
 export default Footer;
