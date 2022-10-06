2a3f34d0bf3e8211905fdcc6dcde255c5e5efd25
Dave Landey <davelandey@gmail.com>
2022-10-06 19:37:00 -0400

    header css added

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,22 +1,22 @@
 import React from "react";
 import "./Header.css";
 import { NavLink, Container, Row, Col } from "reactstrap";
-const fireHydrant = require("../../images/Fire-Hydrant.jpeg");
+const fireHydrant = require("../../images/Hydrant-3.png");
 
 const Header = (props) => {
   return (
     <>
       <Container className="header-bar" fluid>
         <Row>
-          <Col className ="headerCol1" xs="1">
+          <Col className ="headerCol1" xs="2">
           <NavLink to='/'> <img className="hydrant" src={fireHydrant} alt="A cartoon fire hydrant"/></NavLink>
           </Col>
           <Col xs="auto">
             <h1 className="pupSpot-text">PupSpot.</h1>
             <h2 className="headerTagLine">The most unbelivebly catchy tagline, known to humankind</h2>
           </Col>
-        </Row>
-      </Container>
+          </Row>
+            </Container>
     </>
   );
 };
