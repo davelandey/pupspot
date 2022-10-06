924ca0049b714881dfc14f35e65e3d0d4fb7bf10
Elena Bilodeau <elenabilodeau@gmail.com>
2022-10-06 16:48:34 -0400

    completed navbar component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,37 +1,38 @@
-import './App.css';
+import "./App.css";
 import { Routes, Route, NavLink } from "react-router-dom";
-import Header from './components/Header';
-import Navbar from './components/Navbar';
-import Signup from './components/Signup';
-import Login from './components/Login';
-import Footer from './components/Footer';
-import Map from './components/Map';
-import Profile from './components/Profile';
+import Header from "./components/Header";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+import Footer from "./components/Footer";
+import Map from "./components/Map";
+import Profile from "./components/Profile";
+import NavbarComponent from "./components/NavbarComponent";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
+      <NavbarComponent />
       <p className="pupSpot-text">PupSpot</p>
       <h1>Heading 1</h1>
       <h2>Heading 2</h2>
       <h3>Heading 3</h3>
- 
+
       <Header />
-      <Navbar />
       <Login />
       <Signup />
       <Profile />
       <Map />
       <Footer />
 
-          {/* <Routes>
-          <Route path="/" element={< />} />
-          <Route path="/" element={< />} />
-          <Route path="/" element={< />} />
-          <Route path="/" element={< />} />
-          <Route path="/" element={< />} />
-          <Route path="/" element={< />} />
-        </Routes> */}
+      <Routes>
+        <Route path="/location" element={<Map />} />
+        <Route path="/dog-parks" element={<Map />} />
+        <Route path="/trails" element={<Map />} />
+        <Route path="/restaurants" element={<Map />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
     </div>
   );
 }
