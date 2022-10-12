cece3c81f485b818658fe7662d4eeb9c91aa6418
Elena Bilodeau <elenabilodeau@gmail.com>
2022-10-12 16:25:15 -0400

    started on individual map pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,17 +3,18 @@ import { Routes, Route, NavLink } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Signup from "./components/Auth/Signup/Signup";
 import Footer from "./components/Footer/Footer";
-import Map from "./components/Map/Map";
 import Profile from "./components/Profile/ProfilePage";
 import PetProfile from "./components/PetProfile/PetProfilePage";
 import NavbarComponent from "./components/NavbarComponent/NavbarComponent";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import Map from "./components/Map/Map";
 import { useState, useEffect } from "react";
 import React from "react";
 import { Endpoints } from "./components/Routes/Endpoints";
 import { RouteFetch } from "./components/Routes";
 import Auth from "./components/Auth/Auth";
+import IndividualLocation from "./components/Map/IndividualLocation";
+import Home from "./components/Home/Home";
 
 function App() {
   //*----TOKEN----
@@ -37,13 +38,13 @@ function App() {
     console.log(newToken);
   };
 
-  const protectedViews = () => {
-    return localStorage.getItem("token") === sessionToken ? (
-      <Map token={sessionToken} />
-    ) : (
-      <Auth updateToken={updateToken} />
-    );
-  };
+  // const protectedViews = () => {
+  //   return localStorage.getItem("token") === sessionToken ? (
+  //     <Map token={sessionToken} />
+  //   ) : (
+  //     <Auth updateToken={updateToken} />
+  //   );
+  // };
 
   const [locations, setLocations] = useState([]);
 
@@ -64,30 +65,45 @@ function App() {
   };
 
   useEffect(() => {
-    // fetchLocations();
+    fetchLocations();
   }, []);
 
-  console.log(locations);
+  // console.log(locations);
 
   return (
     <div className="App">
       <Header />
-
-      <NavbarComponent sessionToken={sessionToken} clearToken={clearToken} updateToken={updateToken} />
+      <NavbarComponent
+        sessionToken={sessionToken}
+        clearToken={clearToken}
+        updateToken={updateToken}
+      />
       {/* <Profile/> */}
-      <Map />
+
+      <div className="content-section">
+        <Routes>
+          <Route path="/" element={<Home locations={locations}/>} />
+          <Route path="/location" element={<Map locations={locations} />} />
+          <Route path="/dog-parks" element={<Map locations={locations} />} />
+          <Route path="/trails" element={<Map locations={locations} />} />
+          <Route path="/restaurants" element={<Map locations={locations} />} />
+          {/* Creates path based on location name */}
+          <Route
+            path="/:locationName"
+            element={<IndividualLocation locations={locations} />}
+          />
+          <Route
+            path="/signup"
+            element={<Signup updateToken={updateToken} />}
+          />
+          <Route path="/user-profile" element={<Profile />} />
+          <Route path="/pet-profile" element={<PetProfile />} />
+        </Routes>
+      </div>
+
       <Footer />
 
       <Footer />
-      <Routes>
-        <Route path="/location" element={<Map />} />
-        <Route path="/dog-parks" element={<Map />} />
-        <Route path="/trails" element={<Map />} />
-        <Route path="/restaurants" element={<Map />} />
-        <Route path="/signup" element={<Signup updateToken={updateToken}/>} />
-        <Route path="/user-profile" element={<Profile />} />
-        <Route path="/pet-profile" element={<PetProfile />} />
-      </Routes>
     </div>
   );
   {
